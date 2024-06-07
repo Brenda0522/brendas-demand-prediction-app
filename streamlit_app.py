4e3b39261f5898394aa332552fec28907239a1a6
Brenda Liu <65971398+Brenda0522@users.noreply.github.com>
2024-06-07 09:09:11 -0700

    create app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,98 @@
-import altair as alt
-import numpy as np
-import pandas as pd
 import streamlit as st
+import pandas as pd
+from pathlib import Path
+import numpy as np
+from sklearn.linear_model import LinearRegression
 
-"""
-# Welcome to Streamlit!
+st.set_page_config(page_title='Superman Demand Estimation')
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+@st.cache_data
+def get_data():
+    DATA_FILENAME = Path(__file__).parent/'data/AMR_Data.csv'
+    df_amr = pd.read_csv(DATA_FILENAME)
+    DATA_FILENAME = Path(__file__).parent/'data/EU_Data.csv'
+    df_eu = pd.read_csv(DATA_FILENAME)
+    DATA_FILENAME = Path(__file__).parent/'data/PAC_Data.csv'
+    df_pac = pd.read_csv(DATA_FILENAME)
+    
+    return df_amr, df_eu, df_pac
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+def clear_input():
+    st.session_state.actual = ''
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+df_amr, df_eu, df_pac = get_data()
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+amr = st.checkbox('AMR', on_change=clear_input)
+eu = st.checkbox('Europe', on_change=clear_input)
+pac = st.checkbox('PAC', on_change=clear_input)
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
+df = pd.DataFrame(columns=['week', 'year_2', 'year_1'])
+df['week'] = [0] * 20
+df['year_2'][:15] = 0.
+df['year_2'][15:] = np.NaN
+df['year_1'][:5] = np.NaN
+df['year_1'][5:] = 0.
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
+if amr:
+    df += df_amr
+if eu:
+    df += df_eu
+if pac:
+    df += df_pac
+df['week'] = np.arange(1, 21, 1)
 
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+if amr or eu or pac:
+    # Calculate price factor
+    year_2_normalized = np.mean(df['year_2'][2:15])
+    year_1_normalized = np.mean(df['year_1'][7:20])
+    price_factor = (year_2_normalized - year_1_normalized) / (120 - 200)
+
+    # Display slider for year_1 weight
+    year_1_weight = st.slider(label='Last year seasonality weight', min_value=0., max_value=1., value=0.7)
+
+    # Make estimate
+    df['year_0_estimate'] = np.NaN
+    df['year_0_estimate'][:7] = df['year_2'][:7] + price_factor * (205 - 120)
+    estimate_2 = df['year_2'][7:15] + price_factor * (205 - 120)
+    estimate_1 = df['year_1'][7:15] + price_factor * (205 - 200)
+    df['year_0_estimate'][7:15] = estimate_1 * year_1_weight + estimate_2 * (1 - year_1_weight)
+
+    # Display input field for actual data
+    st.text_input(label='Current year actual demand, separated by comma, 15 inputs max', placeholder='240,200,150', key='actual', on_change=None)
+
+    # Update estimate
+    try:
+        # Validate input
+        actual_year_0 = [float(s.strip()) for s in st.session_state.actual.split(',')]
+        assert(len(actual_year_0) <= 15)
+        for value in actual_year_0:
+            assert(value >= 0.)
+
+        # Fit new price factor
+        actual_mean = np.mean(actual_year_0)
+        model = LinearRegression()
+        model.fit(X=[[120.], [200.], [205.]], y=[[year_2_normalized], [year_1_normalized], [actual_mean]])
+        price_factor_new = model.coef_[0]
+
+        # Make new estimates
+        df['year_0_estimate'][:len(actual_year_0)] = actual_year_0
+        if len(actual_year_0) < 7:
+            # Use year_2 for first portion and both for second portion
+            df['year_0_estimate'][len(actual_year_0):7] = df['year_2'][len(actual_year_0):7] + price_factor_new * (205 - 120)
+            estimate_2 = df['year_2'][7:15] + price_factor_new * (205 - 120)
+            estimate_1 = df['year_1'][7:15] + price_factor_new * (205 - 200)
+            df['year_0_estimate'][7:15] = estimate_1 * year_1_weight + estimate_2 * (1 - year_1_weight)
+        else:
+            # Use both for the second portion
+            estimate_2 = df['year_2'][len(actual_year_0):15] + price_factor_new * (205 - 120)
+            estimate_1 = df['year_1'][len(actual_year_0):15] + price_factor_new * (205 - 200)
+            df['year_0_estimate'][len(actual_year_0):15] = estimate_1 * year_1_weight + estimate_2 * (1 - year_1_weight)
+    except:
+        pass
+
+    # Draw the graph
+    st.line_chart(df, x='week', y=['year_2', 'year_1', 'year_0_estimate'], color=('#ff0000', '#7777ff', '#2222ff'))
+
+    # Display estimate
+    if st.checkbox('Display all estimates'):
+        df[['week', 'year_0_estimate']][:15]
